2662c09d314eddd5c9a30e0bb3de6edb300eaccd
Weiyao Luo <wyluo@thoughtworks.com>
2018-07-28 16:55:56 +0800

    Initial commit

---
+++ src/main/java/com/thoughtworks/tw55_demo/Tw55DemoApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.tw55_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tw55DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tw55DemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/thoughtworks/tw55_demo/Tw55DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.tw55_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tw55DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
